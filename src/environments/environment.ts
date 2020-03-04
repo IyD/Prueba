53f78a50395b4296ddaab0ac2ea988283b6e2b7a
Jonathan Landin <jonathan.gits@gmail.com>
2020-03-04 17:10:28 -0600

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCC4Yv6GTi7ukiFF2QLOEbIsfDTjCjYsio",
+    authDomain: "careful-record-147700.firebaseapp.com",
+    databaseURL: "https://careful-record-147700.firebaseio.com",
+    projectId: "careful-record-147700",
+    storageBucket: "careful-record-147700.appspot.com",
+    messagingSenderId: "553796179230",
+    appId: "1:553796179230:web:5526c1be6b76921ef5250f",
+    measurementId: "G-4P6GBZW763"
+  }
 };
 
 /*
